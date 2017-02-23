a485918e324d1dda852556d87f692a6e0df20c66
iggym <iggy.mw@gmail.com>
2017-02-23 08:03:47 -0600

    Added initial project

--- TasksApp/ViewController.swift
+++ TasksApp/ViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = "Our Title"
+        cell.detailTextLabel!.text = "the details are here"
+        return cell
+    }
+    
+  
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
