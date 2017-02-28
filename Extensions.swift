72e297f4baba85ca5993c921b4c8cdc49eeb9eb5
iggym <iggy.mw@gmail.com>
2017-02-27 21:40:10 -0600

    added string extension

--- Extensions.swift
+++ Extensions.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        let first = String(characters.prefix(1)).capitalized
+        let other = String(characters.dropFirst())
+        return first + other
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
